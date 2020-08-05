45c4c1ad0be4088756627004ce610d47b2b40906
Alexander Bogomolov <dummy@mail.ru>
2020-08-05 12:36:23 +1000

    [fix] logout через get запрос

--- src/routes.php
+++ src/routes.php
@@ -3,6 +3,7 @@
 Route::group(['middleware' => ['web']], static function () {
     Route::get('/me', 'S25\Auth\Controllers\LoginController@loginByToken');
     Route::get('/token', 'S25\Auth\Controllers\LoginController@loginByToken');
+    Route::get('/logout', 'S25\Auth\Controllers\LoginController@logout');
     Route::post('/logout', 'S25\Auth\Controllers\LoginController@logout');
     Route::get('/api/user', 'S25\Auth\Controllers\UserController@user');
 });
